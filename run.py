a067104b86e1c61a53236a1dcb488942e8a1363c
RuneMasterGaming <runemaster580@gmail.com>
2016-10-26 19:36:22 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,19 @@
+import os, time, urllib.request
+cversion 0.1
+urllib.request.urlretrieve("https://raw.githubusercontent.com/RuneMasterGaming/manager/master/version.txt", "version.txt")
+nversion = open('version.txt', 'r')
+nversion = float(nversion.read())
+if nversion > cversion:
+    for filename in os.listdir("."):
+        if filename.startswith("run"):
+            os.rename(filename, "old_run.py")
+            print("renamed old manager")
+            time.sleep(1)
+            urllib.request.urlretrieve("https://raw.githubusercontent.com/RuneMasterGaming/manager/master/run.py", "run.py")
+    os.system("nohup python run.py &")
+    time.sleep(1)
+    os.kill(os.getppid(), signal.SIGHUP)
+elif nversion == cversion:
+    print("Currently up to date!")
+else:
+    print("Are you some sort of time traveler?")
