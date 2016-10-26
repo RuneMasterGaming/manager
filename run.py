223a9ae7add0c54154c498aeb6dcb0ef0566b716
RuneMasterGaming <runemaster580@gmail.com>
2016-10-26 19:52:21 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os, time, urllib.request
-cversion = 0.2
+cversion = 0.3
 urllib.request.urlretrieve("https://raw.githubusercontent.com/RuneMasterGaming/manager/master/version.txt", "version.txt")
 nversion = open('version.txt', 'r')
 nversion = float(nversion.read())
