5ad6afbe2a94b732e9cc390f9e742d09a0d48d64
RuneMasterGaming <runemaster580@gmail.com>
2016-10-27 10:32:27 -0400

    Update run.py

--- run.py
+++ run.py
@@ -14,9 +14,8 @@ def updater():
                 print("renamed old manager")
                 time.sleep(1)
                 urllib.request.urlretrieve("https://raw.githubusercontent.com/RuneMasterGaming/manager/master/run.py", "run.py")
-        os.system("nohup python run.py &")
         time.sleep(1)
-        os.kill(os.getppid(), signal.SIGHUP)
+        os.system("python run.py")
     elif nversion == cversion:
         print("Currently up to date on version" + str(cversion))
     else:
